7a1f1fd3932d280618c56da3ef20f5d1d3439bfd
Connie Ip <connie.ip@cargosmart.com>
2018-11-20 22:45:42 +0800

    testcase end number smaller than 1

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,7 @@ const createMultiplicationTable = require('../main');
 it("should output null if end number larger than start number", () => {
     expect(createMultiplicationTable(5, 4)).toBe(null);
 });
+
+it("should output null if end number smaller than 1", () => {
+    expect(createMultiplicationTable(0, 3)).toBe(null);
+});
