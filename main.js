16cec28cb5173480b7287bf0c112627a8716475b
Connie Ip <connie.ip@cargosmart.com>
2018-11-20 22:54:31 +0800

    implement create multiply table

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ function createMultiplicationTable(start, end) {
     if (!checkInputValid(start, end)) {
         return null;
     }
+
+    return createMultiplicationTableWithValidNumbers(start, end);
+}
+
+function printMultiplyEquation(a, b, result) {
+    return (a + '*' + b + '=' + result);
 }
 
 function checkInputValid(start, end) {
@@ -12,6 +18,28 @@ function checkInputValid(start, end) {
         return false;
     }
     return true;
+}
+
+function createMultiplicationTableWithValidNumbers(start, end) {
+    let i;
+    let j;
+    let text = '';
+    for (i = start; i <= end; i++) {
+        let line = '';
+        for (j = start; j <= end; j++) {
+            if (j <= i) {
+                if (line.length != 0) {
+                    line += '  ';
+                }
+                line += printMultiplyEquation(j, i, multiply(j * i));
+            }
+        }
+        text += line;
+        if (i != end) {
+            text += '\n';
+        }
+    }
+    return text;
 }
 
 module.exports = createMultiplicationTable;
