de44af09a9148a285e3ed82fcf5ef83683500dee
Connie Ip <connie.ip@cargosmart.com>
2018-11-20 22:50:19 +0800

    testcase start num or end num smaller than 1

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -4,6 +4,10 @@ it("should output null if end number larger than start number", () => {
     expect(createMultiplicationTable(5, 4)).toBe(null);
 });
 
-it("should output null if end number smaller than 1", () => {
+it("should output null if start number smaller than 1", () => {
     expect(createMultiplicationTable(0, 3)).toBe(null);
+});
+
+it("should output null if end number smaller than 1", () => {
+    expect(createMultiplicationTable(0, 0)).toBe(null);
 });
