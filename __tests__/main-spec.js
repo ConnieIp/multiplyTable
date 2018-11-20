33325b6f72c435f7fef187c1823f350654b60df7
Connie Ip <connie.ip@cargosmart.com>
2018-11-20 22:51:57 +0800

    testcase start num or end num larger than 1000

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -11,3 +11,11 @@ it("should output null if start number smaller than 1", () => {
 it("should output null if end number smaller than 1", () => {
     expect(createMultiplicationTable(0, 0)).toBe(null);
 });
+
+it("should output null if start number larger than 1000", () => {
+    expect(createMultiplicationTable(1001, 1001)).toBe(null);
+});
+
+it("should output null if end number larger than 1000", () => {
+    expect(createMultiplicationTable(5, 1001)).toBe(null);
+});
